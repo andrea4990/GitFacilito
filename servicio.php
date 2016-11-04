d1e0722b1df62cc7bc7230ef6c63191ac4312c1f
Andrea <andreavr04@gmail.com>
2016-11-04 11:53:04 -0400

    primer paso

---
+++ servicio.php
@@ -0,0 +1,38 @@
+<?php
+// API access key from Google API's Console
+define( 'API_ACCESS_KEY','AIzaSyB1N2h16Gd9a7eMdf00Z9if8GN6d3F1Wx8');
+$registrationIds = array( $_GET['id'] );
+// prep the bundle
+$msg = array
+(
+	'message' 	=> 'here is a message',
+	'title'		=> 'This is a title',
+	'subtitle'	=> 'This is a subtitle. subtitle',
+	'tickerText'	=> 'Ticker text here...Ticker text here...Ticker text here',
+	'vibrate'	=> 1,
+	'sound'		=> 1,
+	'largeIcon'	=> 'large_icon',
+	'smallIcon'	=> 'small_icon'
+);
+$fields = array
+(
+	'registration_ids' 	=> $registrationIds,
+	'data'			=> $msg
+);
+ 
+$headers = array
+(
+	'Authorization: key=' . API_ACCESS_KEY,
+	'Content-Type: application/json'
+);
+ 
+$ch = curl_init();
+curl_setopt( $ch,CURLOPT_URL, 'https://android.googleapis.com/gcm/send' );
+curl_setopt( $ch,CURLOPT_POST, true );
+curl_setopt( $ch,CURLOPT_HTTPHEADER, $headers );
+curl_setopt( $ch,CURLOPT_RETURNTRANSFER, true );
+curl_setopt( $ch,CURLOPT_SSL_VERIFYPEER, false );
+curl_setopt( $ch,CURLOPT_POSTFIELDS, json_encode( $fields ) );
+$result = curl_exec($ch );
+curl_close( $ch );
+echo $result;
